ce18f224290d96dd5497c9a30e98c9a2b6984802
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-08-22 15:36:02 -0700

    Added form validation

--- login_app/lib/ui/CustomLoginForm.dart
+++ login_app/lib/ui/CustomLoginForm.dart
@@ -7,18 +7,103 @@ class CustomLoginForm extends StatefulWidget {
 
 class _CustomLoginFormState extends State<CustomLoginForm> {
   final _formKey = GlobalKey<FormState>();
-  
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          icon: new ,
-        ],
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Icon(
+                Icons.local_dining,
+                size: 90,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Name",
+                    border: OutlineInputBorder(
+                      gapPadding: 3.3,
+                      borderRadius: BorderRadius.circular(3.3),
+                    )),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Please enter name";
+                  } else {
+                    print("All is Good");
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      gapPadding: 3.3,
+                      borderRadius: BorderRadius.circular(3.3),
+                    )),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Please enter password";
+                  } else {
+                    print("All is Good");
+                  }
+                },
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text("All is good!")));
+                        }
+                      },
+                      child: Text("Submit"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.reset();
+                        }
+                      },
+                      child: Text("Clear"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Center(
+                child: Text("Hello There",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19.0,
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
-      
     );
   }
 }
