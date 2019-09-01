c10d8f5d18b809f24beeb5d74dfa9836c3773ece
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-08-31 23:46:59 -0700

    nice

--- parsing_data_update/lib/main.dart
+++ parsing_data_update/lib/main.dart
@@ -3,63 +3,73 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 //https://jsonplaceholder.typicode.com/posts
-
+//https://jsonplaceholder.typicode.com/users
 
 void main() async {
   List _data = await getJSON();
 
-  runApp(new MaterialApp(
-    home: new Scaffold(
-      appBar: AppBar(
-        title: Text("JSON Parsing"),
-        backgroundColor: Colors.orange,
-      ),
-      body: ListView.builder(
-        itemCount: _data.length,
-        padding: const EdgeInsets.all(4.4),
-        itemBuilder: (BuildContext context, int position){
-          return Column(
-            children: <Widget>[
-              Divider(height: 13.4,),
-              ListTile(
-                title: Text(_data[position]["title"],
-                  style: TextStyle(
-                    fontSize: 17.2,
-                    fontWeight: FontWeight.bold,
+  runApp(
+    new MaterialApp(
+        home: new Scaffold(
+            appBar: AppBar(
+              title: Text("JSON Parsing"),
+              backgroundColor: Colors.orange,
+            ),
+            body: ListView.builder(
+              itemCount: _data.length,
+              padding: const EdgeInsets.all(4.4),
+              itemBuilder: (BuildContext context, int position) {
+                return Column(
+                  children: <Widget>[
+                    Divider(
+                      height: 13.4,
                     ),
-                  ),
-                subtitle: Text(_data[position]["body"]),
-                leading: CircleAvatar(
-                  backgroundColor: Colors.orange,
-                  child: Text(_data[position]["title"][0]),
-                  ),
-
-                onTap: () => print("Hello ${_data[position]["id"]}"),
-                ),
-              
-
-            ],
-          );
-        },
-      )
-    )
-  ),
+                    ListTile(
+                      title: Text(
+                        _data[position]["name"],
+                        style: TextStyle(
+                          fontSize: 17.2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(_data[position]["email"]),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.orange,
+                        child: Text(_data[position]["name"][0]),
+                      ),
+                      onTap: () => showTapMessage(context, 
+                      _data[position]["address"]["street"] + " " 
+                      + _data[position]["address"]["suite"] + "\n"
+                      + _data[position]["address"]["geo"]["lat"]),
+                    ),
+                  ],
+                );
+              },
+            ))),
   );
 }
 
-void showTapMessage (BuildContext context, int position)
-{
+void showTapMessage(BuildContext context, String message) {
+  
   var alertDialog = new AlertDialog(
-    title: Text("Hello"),
+    title: Text(message),
     actions: <Widget>[
-      FlatButton(child: Text("Ok"), onPressed: () => debugPrint("OK pressed"),),
-
+      FlatButton(
+        child: Text("Ok"),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
     ],
   );
+
+  showDialog(
+      context: context,
+      builder: (context) {
+        return alertDialog;
+      });
 }
 
 Future<List> getJSON() async {
-  String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  String apiUrl = "https://jsonplaceholder.typicode.com/users";
   http.Response response = await http.get(apiUrl);
 
   return json.decode(response.body);
